9bca6ceb3be22a1b2fef83f9d82163fe0f957ce6
Maniack Crudelis <maniackcrudelis@users.noreply.github.com>
2017-01-19 01:21:01 +0100

    Guillemets supprimés pour _common

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Version cible de la mise à jour de Nextcloud
-VERSION="11.0.1"
+VERSION=11.0.1
 
 # Nextcloud tarball checksum sha256
-NEXTCLOUD_SOURCE_SHA256="00162bf454914a2acbe6a9ac47c9db9f411b99064f0736b43e73cabbd87f4629"
+NEXTCLOUD_SOURCE_SHA256=00162bf454914a2acbe6a9ac47c9db9f411b99064f0736b43e73cabbd87f4629
 
 # Load common variables and helpers
 source ./_common.sh

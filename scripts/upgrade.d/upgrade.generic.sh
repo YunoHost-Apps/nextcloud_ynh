5bb0a5430020324a6e037075fd0f2d258532b27a
Jimmy Monin <jimmy@monin.net>
2017-04-17 18:08:12 +0200

    Revert "Fix temp directory rights on upgrade"
    This reverts commit 89a336801f7452b9037f6409f095ef5ef9efcb77.

--- scripts/upgrade.d/upgrade.generic.sh
+++ scripts/upgrade.d/upgrade.generic.sh
@@ -17,9 +17,7 @@ COMMON_UPGRADE () {
 
 	# Retrieve new Nextcloud sources in a temporary directory
 	TMPDIR=$(mktemp -d)
-	# Set temp folder ownership
-	sudo chown -R $app: "$TMPDIR"
-	extract_nextcloud "$TMPDIR"	"$app" # Télécharge nextcloud, vérifie sa somme de contrôle et le décompresse.
+	extract_nextcloud "$TMPDIR"	# Télécharge nextcloud, vérifie sa somme de contrôle et le décompresse.
 
 	# Copy Nextcloud configuration file
 	sed -i "s@#DOMAIN#@${domain}@g" ../conf/config.json

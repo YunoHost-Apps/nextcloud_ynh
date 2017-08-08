2f3c7e25b325c66c4d6b8549d0d5286f3cbcf327
Rafi59 <rafi59_dev@srvmaison.fr.nf>
2017-08-08 14:50:44 +0200

    Update upgrade.last.sh

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -5,6 +5,7 @@ VERSION=12.0.1
 
 # Nextcloud tarball checksum sha256
 NEXTCLOUD_SOURCE_SHA256=5288f645348eddc1a7768825678bd19f110cec585a16f98b52c64389358c74bc
+
 # Load common variables and helpers
 source ./_common.sh
 

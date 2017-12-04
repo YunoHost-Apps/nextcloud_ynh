0938148aac4d3d9f1e1208e5b07e070830305d30
Jimmy Monin <jimmy@monin.net>
2017-12-04 21:36:01 +0100

    Upgrade to upstream version 12.0.4

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Last available nextcloud version
-next_version="12.0.3"
+next_version="12.0.4"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="88bcaccba886d0e5a145b15fe216d652ab68a0a4c089a102f1fa1e78e6ddfb71"
+nextcloud_source_sha256="654161a74ceaf9a60c7731d7d6702e6710a972633a97955d16f01abeb09d09b6"

3fdcdf6576a62b26c232ff415c68ba0a64abca42
Jimmy Monin <jimmy@monin.net>
2018-05-14 18:53:02 +0200

    Fix automatic logout from YunoHost when logging out from Nextcloud (#106): - fix Nextcloud source patch - fix patches not applied on installation - add an empty patches directory to work around a nasty cache issue when running package_check

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -5,3 +5,6 @@ next_version="13.0.1"
 
 # Nextcloud tarball checksum sha256
 nextcloud_source_sha256="5743314a71e972ae46a14b36b37394d4545915aa5f32d9e12ba786d04c1f1d11"
+
+# Patch nextcloud files only for the last version
+cp -a ../sources/patches_last_version/* ../sources/patches

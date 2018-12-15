53c4fb686b427fd72fa5686e80efd786b49ac1c4
Nathanael <nathanael@hannebert.com>
2018-12-15 12:04:09 +0100

    Upgrade to upstream version 15.0.0
    Upgrade to upstream version 15.0.0

--- scripts/upgrade.d/upgrade.14.sh
+++ scripts/upgrade.d/upgrade.14.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Last available nextcloud version
-next_version="15.0.0"
+next_version="14.0.4"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="5bb0c58171353da844019b64080c21078002a59ab956ab72adb958844a98eb78"
+nextcloud_source_sha256="4f5dd15a71694bd2f15fba0d2f942e5a5b1f5aba13511c507a23324d746b40e8"
 
 # Patch nextcloud files only for the last version
 cp -a ../sources/patches_last_version/* ../sources/patches

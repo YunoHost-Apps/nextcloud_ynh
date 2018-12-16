698218cacd4b4e77e55207d7bf60bf8a2389b981
Jimmy Monin <jimmy@monin.net>
2018-12-16 18:02:59 +0100

    Plan required maintenance operations after upgrade

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -8,3 +8,6 @@ nextcloud_source_sha256="4f5dd15a71694bd2f15fba0d2f942e5a5b1f5aba13511c507a23324
 
 # Patch nextcloud files only for the last version
 cp -a ../sources/patches_last_version/* ../sources/patches
+
+# Execute post-upgrade operations later on
+(cd /tmp ; at now + 10 minutes <<< "(cd $final_path ; sudo -u nextcloud php occ db:add-missing-indices ; sudo -u nextcloud php occ db:convert-filecache-bigint -n) > /tmp/nextcloud_maintenance.log")

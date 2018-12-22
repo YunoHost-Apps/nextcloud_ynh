a9bb825abb4075103094af3f0ea0013d8fc9ee4d
Jimmy Monin <jimmy@monin.net>
2018-12-22 09:26:05 +0100

    Limit upgrade operations (patches, maintenance, etc.) to the last upgrade

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -6,8 +6,11 @@ next_version="15.0.0"
 # Nextcloud tarball checksum sha256
 nextcloud_source_sha256="5bb0c58171353da844019b64080c21078002a59ab956ab72adb958844a98eb78"
 
-# Patch nextcloud files only for the last version
-cp -a ../sources/patches_last_version/* ../sources/patches
+# This function will only be executed upon applying the last upgrade referenced above
+last_upgrade_operations () {
+  # Patch nextcloud files only for the last version
+  cp -a ../sources/patches_last_version/* ../sources/patches
 
-# Execute post-upgrade operations later on
-(cd /tmp ; at now + 10 minutes <<< "(cd $final_path ; sudo -u nextcloud php occ db:add-missing-indices ; sudo -u nextcloud php occ db:convert-filecache-bigint -n) > /tmp/nextcloud_maintenance.log")
+  # Execute post-upgrade operations later on
+  (cd /tmp ; at now + 10 minutes <<< "(cd $final_path ; sudo -u nextcloud php occ db:add-missing-indices ; sudo -u nextcloud php occ db:convert-filecache-bigint -n) > /tmp/nextcloud_maintenance.log")
+}

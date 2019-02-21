e8b332662571933550b8be3bf5f489ffdeba26e8
Mickaël Martin <mickael@librement-votre.fr>
2019-02-21 18:24:14 +0100

    Revert "Major upgrade to upstream version 14.0.4"
    This reverts commit 3f5de23ba340c21117f7a4aa636fcb2e416b3ed0.

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Last available nextcloud version
-next_version="14.0.4"
+next_version="13.0.6"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="4f5dd15a71694bd2f15fba0d2f942e5a5b1f5aba13511c507a23324d746b40e8"
+nextcloud_source_sha256="90fc9e960b6a477bb14ee87042b3d158bde95c3f0157677cb4547ca7649968d4"
 
 # Patch nextcloud files only for the last version
 cp -a ../sources/patches_last_version/* ../sources/patches

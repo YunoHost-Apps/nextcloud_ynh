572c6b4254adecbf4e100b8a4a247d226efede0f
Jimmy Monin <jimmy@monin.net>
2019-05-20 22:10:02 +0200

    Upgrade to upstream version 15.0.8

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Last available nextcloud version
-next_version="15.0.7"
+next_version="15.0.8"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="3e6158951fa72010ccd50dbeac05d8df162183f7bbc62a1c6c89ed7081fa9d49"
+nextcloud_source_sha256="b782599fa39919ecd96d93cfb6374f4d42cd6de22a9a2d12ec11ed38a2e5f2f0"
 
 # This function will only be executed upon applying the last upgrade referenced above
 last_upgrade_operations () {

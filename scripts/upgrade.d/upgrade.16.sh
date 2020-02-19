5310fd9f3ac905f7a565cdf4b4b4c8db46e9bd86
Jimmy Monin <jimmy@monin.net>
2020-02-19 07:11:40 +0100

    Fix upgrade from previous version

--- scripts/upgrade.d/upgrade.16.sh
+++ scripts/upgrade.d/upgrade.16.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 # Last available nextcloud version
-next_version="16.0.5"
+next_version="17.0.0"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="8709c64fa776fd731c8e5f1ab25d592a2e690e5e18a81601cccf363795fae551"
-
-# This function will only be executed upon applying the last upgrade referenced above
-last_upgrade_operations () {
-  # Patch nextcloud files only for the last version
-  cp -a ../sources/patches_last_version/* ../sources/patches
-
-  # Execute post-upgrade operations later on
-  (cd /tmp ; at now + 10 minutes <<< "(cd $final_path ; sudo -u $app php occ db:add-missing-indices ; sudo -u $app php occ db:convert-filecache-bigint -n) > /tmp/${app}_maintenance.log")
-}
+nextcloud_source_sha256="6081421b33ecdb3130b2bfb2293a3f4045aeb0b471ee570e675de3d931a142a6"

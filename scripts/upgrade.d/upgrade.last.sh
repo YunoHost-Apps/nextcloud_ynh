3251079286e68987e5191aa6e01c1a47c74cc78d
Jimmy Monin <jimmy@monin.net>
2020-06-14 15:23:05 +0200

    Upgrade to upstream version 19.0.0

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Last available nextcloud version
-next_version="18.0.4"
+next_version="19.0.0"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="fad8e12632b352247ffc5ae181d4e414d732b9072caa0401774cfdb93a714329"
+nextcloud_source_sha256="d23d429657c5e3476d7e73af1eafc70e42a81cfe2ed65b20655a005724fe0aae"
 
 # This function will only be executed upon applying the last upgrade referenced above
 last_upgrade_operations () {
@@ -12,5 +12,5 @@ last_upgrade_operations () {
   cp -a ../sources/patches_last_version/* ../sources/patches
 
   # Execute post-upgrade operations later on
-  (cd /tmp ; at now + 10 minutes <<< "(cd $final_path ; sudo -u $app php${YNH_PHP_VERSION} occ db:add-missing-indices ; sudo -u $app php${YNH_PHP_VERSION} occ db:convert-filecache-bigint -n) > /tmp/${app}_maintenance.log")
+  (cd /tmp ; at now + 10 minutes <<< "(cd $final_path ; sudo -u $app php${YNH_PHP_VERSION} occ db:add-missing-indices ; sudo -u $app php${YNH_PHP_VERSION} occ db:add-missing-columns ; sudo -u $app php${YNH_PHP_VERSION} occ db:convert-filecache-bigint -n) > /tmp/${app}_maintenance.log")
 }

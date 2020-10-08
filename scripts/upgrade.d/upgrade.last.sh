de251cebfcba1f7da2bf427f168a0edb397d6524
ericgaspar <junk.eg@free.fr>
2020-10-08 10:57:22 +0200

    Update upgrade.last.sh

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Last available nextcloud version
+# Last available Nextcloud version
 next_version="19.0.4"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256=""
+nextcloud_source_sha256="465711715d64cbdf0465fcb4405f23b6f0947b85bbe12b6577c9e1602c63ae78"

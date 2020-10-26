0207a89fe6781a1db59d2df57189564053e70b74
Kay0u <pierre@kayou.io>
2020-10-26 12:03:17 +0100

    20.0.1

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Last available nextcloud version
-next_version="20.0.0"
+next_version="20.0.1"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="a273e0151f68567f729d9a82a7e3f124ff0f0471aa17bae6bfd83c5362d84cd8"
+nextcloud_source_sha256="96c6b50e1e676e46cec82d8f8ff1abd5eef9dfa00580081b6c640612c3ff2efc"

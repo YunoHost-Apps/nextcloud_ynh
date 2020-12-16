dc3590aa78a07d0aa3f776f516d037564e9273d9
ericgaspar <junk.eg@free.fr>
2020-12-16 13:13:57 +0100

    Fix shasum ... :/

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -4,4 +4,4 @@
 next_version="20.0.4"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="4c2857d328b64d12fe283ec2f7030131ebe6e467dd4a033cf602fd07984a28a3"
+nextcloud_source_sha256="269f1622e326f5d11e387d3861aad4e2b0e79334ae97eed5a7b3352ba7661420"

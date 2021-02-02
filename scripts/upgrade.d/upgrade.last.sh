ad61ac01c45dc8ef1618aa61a76c371334bf1222
ericgaspar <junk.eg@free.fr>
2021-02-02 22:25:35 +0100

    Upgrade to version 20.0.7

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Last available Nextcloud version
-next_version="20.0.6"
+next_version="20.0.7"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="859167170402b876b6ef1a37fa4aaa5617b6bf847bb5d50a94f636bae65a34b9"
+nextcloud_source_sha256="8ced82b772bf0af67d5be1323e40f977429bc0a2bcc864095efc78767500b72b"

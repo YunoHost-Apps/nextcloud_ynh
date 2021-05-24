de5a3aab33848270ae90847c01ad471a70f6139e
ericgaspar <junk.eg@free.fr>
2021-05-24 09:43:29 +0200

    21.0.2

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Last available Nextcloud version
-next_version="21.0.1"
+next_version="21.0.2"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="dd7c8ccc01547914a75b44bbf86028289c8919dc39f4e2e720147b6bd596aebe"
+nextcloud_source_sha256="5e5b38109a3485db5fd2d248f24478eabe6c0790ec10b030acbbee207d5511fe"

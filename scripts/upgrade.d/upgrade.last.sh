bd544bfed9eefe926807e94aeb331201cab63ecc
ericgaspar <junk.eg@free.fr>
2021-08-05 13:19:07 +0200

    Update upgrade.last.sh

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -4,4 +4,4 @@
 next_version="22.1.0"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="cbe1ebd3133e2efa6d5b9c0d0f34dc50c1ec45fe042ca06d034cbf15f888dd25"
+nextcloud_source_sha256="4820808f799146853604e1fa27d7f292449018a44dc73bf928a97b02028318ba"

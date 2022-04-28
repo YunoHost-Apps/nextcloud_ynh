ee0092579a0eff78df0425dc4f736fe2ba6aa586
ljf <ljf+git@grimaud.me>
2022-04-28 06:56:04 +0200

    [security] 22.2.7

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Last available Nextcloud version
-next_version="22.2.3"
+next_version="22.2.7"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="66a29a6a490738c0abedb677cb68c7c91fabab3e6419a3c99d8b406899eb942a"
+nextcloud_source_sha256="5ada41cb3e69665e8a13946f71978829c0a0163d0277a49e599c9e8ccf960eab"

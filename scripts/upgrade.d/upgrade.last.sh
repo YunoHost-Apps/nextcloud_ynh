30a649c520951057f3efd2ab74241bb25bc1a16f
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-05-26 09:02:20 +0200

    26.0.2

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Last available Nextcloud version
-next_version="26.0.1"
+next_version="26.0.2"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="6f9c6a1248d7c8af4ad473d73a42565f6adabad4531c5cfa57bc3497b2b64f48"
+nextcloud_source_sha256="f3db0ec5e0aaff7c088eb34f752d77d79913bc6784e0fc47a84cdaa28e567a33"

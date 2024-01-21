7a1481de5a4d59818af451da9ae82e5af261333a
Alexandre Aubin <alex.aubin@mailoo.org>
2024-01-21 19:03:37 +0100

    Use new source format in manifest.toml + stop supporting upgrades from before 22.2

--- scripts/upgrade.d/upgrade.18.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Last available nextcloud version
-next_version="19.0.0"
-
-# Nextcloud tarball checksum sha256
-nextcloud_source_sha256="d23d429657c5e3476d7e73af1eafc70e42a81cfe2ed65b20655a005724fe0aae"

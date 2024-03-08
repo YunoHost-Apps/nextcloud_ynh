77ad98dcd99bab62b603a6e6796597420ba8af36
Félix Piédallu <felix@piedallu.me>
2024-03-08 23:53:12 +0100

    Remove ynh_smart_mktemp, just use a temporary directory just next to the install_dir

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -41,44 +41,5 @@ is_url_handled() {
 }
 
 #=================================================
-
-# Check available space before creating a temp directory.
-#
-# usage: ynh_smart_mktemp --min_size="Min size"
-#
-# | arg: -s, --min_size= - Minimal size needed for the temporary directory, in Mb
-ynh_smart_mktemp () {
-        # Declare an array to define the options of this helper.
-        declare -Ar args_array=( [s]=min_size= )
-        local min_size
-        # Manage arguments with getopts
-        ynh_handle_getopts_args "$@"
-
-        min_size="${min_size:-300}"
-        # Transform the minimum size from megabytes to kilobytes
-        min_size=$(( $min_size * 1024 ))
-
-        # Check if there's enough free space in a directory
-        is_there_enough_space () {
-                local free_space=$(df --output=avail "$1" | sed 1d)
-                test $free_space -ge $min_size
-        }
-
-        if is_there_enough_space /tmp; then
-                local tmpdir=/tmp
-        elif is_there_enough_space /var; then
-                local tmpdir=/var
-        elif is_there_enough_space /; then
-                local tmpdir=/
-        elif is_there_enough_space /home; then
-                local tmpdir=/home
-        else
-                ynh_die "Insufficient free space to continue..."
-        fi
-
-        echo "$(mktemp --directory --tmpdir="$tmpdir")"
-}
-
-#=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================

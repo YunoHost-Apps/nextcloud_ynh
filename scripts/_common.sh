eda419ebdeaab523ee101ef2fd76de12fdc9b2cd
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-11-09 18:56:05 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -23,7 +23,7 @@ wait_nginx_reload() {
 # usage: is_url_handled --domain=DOMAIN --path=PATH_URI
 is_url_handled() {
     # Declare an array to define the options of this helper.
-    #REMOVEME? local legacy_args=dp
+    local legacy_args=dp
     declare -Ar args_array=( [d]=domain= [p]=path= )
     local domain
     local path
